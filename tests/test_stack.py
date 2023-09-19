b050303a5dccdfc8bd821b547664b9c397a6775e
Andrey Emelianenko <lin_user@hppavilion>
2023-09-19 20:21:51 +0300

    add new tests test_stack

---
+++ tests/test_stack.py
@@ -0,0 +1,20 @@
+import pytest
+
+def test_stack():
+    stack = []
+    stack.append("one")
+    stack.append("two")
+
+    assert stack.pop() == "two"
+    assert stack.pop() == "one"
+
+
+def test_emptiness():
+    stack = []
+    assert not stack
+
+    stack.append("one")
+    assert bool(stack) # not not stack
+
+    stack.pop()
+    assert not stack
