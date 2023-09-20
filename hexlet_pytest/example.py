0c7037aef4712086949cb449f04d10184bcfbacb
Andrey Emelianenko <lin_user@hppavilion>
2023-09-20 17:42:32 +0300

    add doctest in example.py

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,17 @@
 def reverse(string):
+    """Reverse string
+
+
+    >>> reverse('')
+    ''
+    >>> reverse('Hello')
+    'olleH'
+    """
+
+
     return string[::-1]
+
+# Нужно для запуска тестов из документации
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
